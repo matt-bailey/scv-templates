4ac5fe2b6a1e4e57cc4f45c76520c216ce0dd308
Matt Bailey <me@mattbailey.co>
2013-05-29 21:34:18 +0100

    Updated nth-child on thumbnails to every fourth item

--- includes/slideshow-product.php
+++ includes/slideshow-product.php
@@ -44,6 +44,54 @@
             </a>
         </div><!-- .item -->
 
+        <div class="item">
+            <a class="fancybox" href="images/slide-2.jpg">
+                <img src="images/slide-2.jpg" />
+            </a>
+        </div><!-- .item -->
+
+        <div class="item">
+            <a class="fancybox" href="images/slide-3.jpg">
+                <img src="images/slide-3.jpg" />
+            </a>
+        </div><!-- .item -->
+
+        <div class="item">
+            <a class="fancybox" href="images/slide-1.jpg">
+                <img src="images/slide-1.jpg" />
+            </a>
+        </div><!-- .item -->
+
+        <div class="item">
+            <a class="fancybox" href="images/slide-2.jpg">
+                <img src="images/slide-2.jpg" />
+            </a>
+        </div><!-- .item -->
+
+        <div class="item">
+            <a class="fancybox" href="images/slide-3.jpg">
+                <img src="images/slide-3.jpg" />
+            </a>
+        </div><!-- .item -->
+
+        <div class="item">
+            <a class="fancybox" href="images/slide-1.jpg">
+                <img src="images/slide-1.jpg" />
+            </a>
+        </div><!-- .item -->
+
+        <div class="item">
+            <a class="fancybox" href="images/slide-2.jpg">
+                <img src="images/slide-2.jpg" />
+            </a>
+        </div><!-- .item -->
+
+        <div class="item">
+            <a class="fancybox" href="images/slide-3.jpg">
+                <img src="images/slide-3.jpg" />
+            </a>
+        </div><!-- .item -->
+
     </div><!-- .carousel-inner -->
 
     <a class="carousel-control left btn btn-inverse" href="#productCarousel" data-slide="prev"><i class="icon-caret-left"></i></a>
